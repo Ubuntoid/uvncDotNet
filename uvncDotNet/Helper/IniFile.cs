7de2519a7762438b7f1b93b2a71846317275c01d
2bite <2bite.ru@gmail.com>
2016-08-23 20:30:15 +0400

    Удалены зависимости от Saltwort

---
+++ uvncDotNet/Helper/IniFile.cs
@@ -0,0 +1,49 @@
+﻿using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace uvncDotNet.Helper
+{
+    public class IniFile
+    {
+        readonly string _path;
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern long WritePrivateProfileString(string section, string key, string value, string filePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern int GetPrivateProfileString(string section, string key, string Default, StringBuilder retVal, int size, string filePath);
+
+        public IniFile(string iniPath)
+        {
+            _path = new FileInfo(iniPath).FullName;
+        }
+
+        public string Read(string key, string section = null)
+        {
+            var retVal = new StringBuilder(255);
+            GetPrivateProfileString(section, key, "", retVal, 255, _path);
+            return retVal.ToString();
+        }
+
+        public void Write(string key, object value, string section = null)
+        {
+            WritePrivateProfileString(section, key, value.ToString(), _path);
+        }
+
+        public void DeleteKey(string key, string section = null)
+        {
+            Write(key, null, section);
+        }
+
+        public void DeleteSection(string section = null)
+        {
+            Write(null, null, section);
+        }
+
+        public bool KeyExists(string key, string section = null)
+        {
+            return Read(key, section).Length > 0;
+        }
+    }
+}
--- uvncDotNet/uvnc/Config.cs
+++ uvncDotNet/uvnc/Config.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.IO;
-using Saltwort.ConfigFile;
+using uvncDotNet.Helper;
 
 namespace uvncDotNet.uvnc
 {
